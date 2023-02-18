a03654d265bd041ecaca7cdbd63380e1fa852e59
grayolaRSA <gtndanga@gmail.com>
2023-02-18 15:52:12 +0200

    update files

--- shell1.c
+++ shell1.c
@@ -12,15 +12,16 @@
  *Return: always zero
  */
 
-extern char **environ;
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	char *command = NULL;
 	size_t len = 0;
 	char *stkn;
 	pid_t my_pid;
-	int status, i;
+	int status, i = 1;
+	char **args;
+
 
 	while (i)
 	{
@@ -28,46 +29,37 @@ int main(int argc, char **argv)
 		getline(&command, &len, stdin);
 
 		stkn = strtok(command, " \n");
-
-		char **arr = malloc(sizeof(char *) * 32);
-
-		arr[0] = stkn;
+		args = malloc(sizeof(char *) * argc);
+		args[0] = stkn;
 
 		i = 1;
 		while (stkn != NULL)
 		{
 			stkn = strtok(NULL, " \n");
-			arr[i] = stkn;
+			argv[i] = stkn;
 			i++;
 		}
 
-		if (strcmp(arr[0], "exit") == 0 && (arr[1] == NULL))
-			exit(0);
+		if (strcmp(args[0], "exit") == 0 && (args[1] == NULL))
+			free(command);
+		free(args);
+		exit(0);
+	}
 
-		if (strcmp(arr[0], "env") == 0 && (arr[1] == NULL))
-		{
-			i = 0;
-			char environ[] = NULL;
-			while (environ[i] != NULL)
-			{
-				printf("%s\n", environ[i]);
-				i++;
-			}
-			continue;
-		}
-		my_pid = fork();
+	my_pid = fork();
+	{
 		if (my_pid == -1)
 		{
 			perror("Error");
 			return (1);
 		}
-
 		else
 			if (my_pid == 0)
 			{
-				if (execve(arr[0], arr, NULL) == -1)
+				args = malloc(sizeof(char *) * argc);
+				if (execve(args[0], args, NULL) == -1)
 				{
-					perror(arr[0]);
+					perror(args[0]);
 					return (1);
 				}
 
@@ -76,8 +68,7 @@ int main(int argc, char **argv)
 			{
 				wait(&status);
 			}
-
+		free(args);
 	}
-	free(command);
 	return (0);
 }
